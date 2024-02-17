5dea4cc3b54c91e884a73d86cc1b0c70e09dafbc
Pilchh <alex@thepilchers.com>
2024-02-17 15:14:44 +0000

    Update moderate service front end

--- ModerateService/api/public/index.js
+++ ModerateService/api/public/index.js
@@ -7,19 +7,19 @@ const updateTypes = async () => {
       result.json().then((result) => {
         typeDropdown.innerHTML = "";
         result.forEach((type) => {
-          let newType = new Option(type.type, type.type_id);
+          let newType = new Option(type.type, type.type);
           typeDropdown.appendChild(newType);
         });
       });
     })
     .catch((err) => console.log(err));
   try {
-    await fetch("http://20.77.67.244/joke/types/").then((result) => {
+    await fetch("https://20.77.67.244/joke/types/").then((result) => {
       if (result.status === 200 || result.status === 304) {
         result.json().then((result) => {
           typeDropdown.innerHTML = "";
           result.forEach((type) => {
-            let newType = new Option(type.type, type.type_id);
+            let newType = new Option(type.type, type.type);
             typeDropdown.appendChild(newType);
           });
         });
@@ -33,14 +33,21 @@ const updateTypes = async () => {
 const submitJoke = async () => {
   const jokeElement = document.getElementById("joke");
   const punchlineElement = document.getElementById("punchline");
+  const typeDropdown = document.getElementById("jokeType");
   const typeElement = document.getElementById("type");
   const pElement = document.getElementById("status");
 
   let data = {
     joke: jokeElement.value,
     punchline: punchlineElement.value,
-    type: typeElement.value,
+    type: "",
   };
+
+  if (typeElement === null) {
+    data.type = typeDropdown.value;
+  } else {
+    data.type = typeElement.value;
+  }
 
   await fetch("add", {
     method: "POST",
@@ -50,7 +57,17 @@ const submitJoke = async () => {
     body: JSON.stringify(data),
   })
     .then(() => {
+      if (typeElement !== null) {
+        typeElement.remove();
+        document.getElementById("type-label").remove();
+      }
+
+      jokeElement.value = "";
+      punchlineElement.value = "";
       pElement.innerText = "Joke added!";
+      const jokeStatus = document.getElementById("jokeStatus");
+
+      jokeStatus.innerText = "Waiting for Joke...";
       isPaused = false;
     })
     .catch((err) => console.log(err));
@@ -60,22 +77,55 @@ const getNext = () => {
   fetch("next").then((data) =>
     data.json().then((joke) => {
       if (joke) {
-        console.log(joke);
         isPaused = true;
-
         const jokeElement = document.getElementById("joke");
         const punchlineElement = document.getElementById("punchline");
-        const typeElement = document.getElementById("type");
+        const typeDropdown = document.getElementById("jokeType");
+        const jokeStatus = document.getElementById("jokeStatus");
 
-        jokeElement.value = joke.joke;
-        punchlineElement.value = joke.punchline;
-        typeElement.value = joke.type;
+        jokeStatus.innerText = "Joke Found";
+
+        jokeElement.value = joke.joke || "";
+        punchlineElement.value = joke.punchline || "";
+        typeDropdown.value = joke.type || "";
       }
     }),
   );
 };
 
+const addTypeInput = () => {
+  const container = document.getElementById("inputs");
+  let label = document.createElement("label");
+  label.innerText = "Create a New Type ";
+  label.id = "type-label";
+  let input = document.createElement("input");
+  input.type = "text";
+  input.id = "type";
+
+  container.appendChild(label);
+  container.appendChild(input);
+};
+
+const deleteJoke = () => {
+  const jokeElement = document.getElementById("joke");
+  const punchlineElement = document.getElementById("punchline");
+  const typeElement = document.getElementById("type");
+  const pElement = document.getElementById("status");
+
+  jokeElement.value = "";
+  punchlineElement.value = "";
+  typeElement.value = "";
+  pElement.innerText = "Joke deleted.";
+
+  const jokeStatus = document.getElementById("jokeStatus");
+
+  jokeStatus.innerText = "Waiting for Joke...";
+
+  isPaused = false;
+};
+
 window.onload = function () {
+  updateTypes();
   setInterval(() => {
     if (!isPaused) {
       getNext();
