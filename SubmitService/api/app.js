13e68eb45581b9888f22ed0594e34fb06c6e5cf9
Pilchh <alex@thepilchers.com>
2024-02-19 15:15:41 +0000

    Implemented authentication to mod and swagger documentation for submit

--- SubmitService/api/app.js
+++ SubmitService/api/app.js
@@ -3,6 +3,29 @@ require("dotenv").config();
 const SubmitRouter = require("./routes/submit.routers");
 const express = require("express");
 const utils = require("./utils");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Submit Service API Documentation",
+      version: "0.0.1",
+      description:
+        "This page outlines all the API endpoints for the Submit Microservice",
+      contact: {
+        name: "Pilchh",
+      },
+    },
+    servers: [
+      {
+        url: "https://20.77.67.244/submit",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
 
 const app = express();
 const PORT = process.env.PORT;
@@ -14,6 +37,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/", SubmitRouter);
+
+const specs = swaggerJsdoc(options);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("*", (_, res) => {
   res.sendStatus(404);
--- SubmitService/api/routes/submit.routers.js
+++ SubmitService/api/routes/submit.routers.js
@@ -1,3 +1,98 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Submit:
+ *       type: object
+ *       required:
+ *         - joke
+ *         - punchline
+ *         - type
+ *       properties:
+ *         joke:
+ *           type: string
+ *           description: The setup line for te joke
+ *         punchline:
+ *           type: string
+ *           description: The punchline for the joke
+ *         type:
+ *           type: string
+ *           description: The type that the joke should be classified as
+ *       example:
+ *         joke: This is a test joke!
+ *         punchline: This is a very funny punchline!
+ *         type: funny
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Submit
+ *   description: The submit joke API
+ * /sub:
+ *   post:
+ *     summary: Submit a new joke
+ *     tags: [Submit]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               joke:
+ *                 type: string
+ *                 example: funny joke setup
+ *               punchline:
+ *                 type: string
+ *                 example: joke punchline
+ *               type:
+ *                 type: string
+ *                 example: funny
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               example: Joke submitted!
+ *       500:
+ *         description: ERROR
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               example: Server error!
+ * /types:
+ *   get:
+ *     summary: Get all joke types
+ *     tags: [Submit]
+ *     responses:
+ *       '200':
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   type_id:
+ *                     type: integer
+ *                     example: 1
+ *                   type:
+ *                     type: string
+ *                     example: funny
+ *       '500':
+ *         description: ERROR
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               example: Server error!
+ */
+
 const express = require("express");
 const SubmitController = require("../controllers/submit.controller");
 const router = express.Router();
